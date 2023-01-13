cb9d0ff7c2e40458c6b074f16ff236cdec19b9ef
SerojEdilyan <sergoedilyan9@gmail.com>
2023-01-13 20:04:35 +0400

    change main

--- Main.java
+++ Main.java
@@ -2,4 +2,9 @@ public class Main {
     public static void main (String[] args) {
         System.out.println("Hello Got!");
     }
+
+    private static void print () {
+        for(int i = 0; i < 5; i++)
+            System.out.println(i);
+    }
 }
